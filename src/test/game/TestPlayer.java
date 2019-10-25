442b7d7b22002d9ddbb1f114ea93eb53fe8918b6
theoricien <agressou@gmail.com>
2019-10-25 08:04:53 +0200

    [ITER1] Tests Unitaires
    Respectivement les tests unitaires sur nos class du jeu "server"-side

---
+++ src/test/game/TestPlayer.java
@@ -0,0 +1,15 @@
+package Game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPlayer {
+	private Player player = new Player();
+	
+	public void testAddFigurineInZone ()
+	{
+		
+	}
+
+}
