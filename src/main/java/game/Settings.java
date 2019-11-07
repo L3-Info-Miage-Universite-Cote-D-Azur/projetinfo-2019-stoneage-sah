69f5badf6ba9f4d8e5ef97e538a134ecf3cde9d2
theoricien <maxime.jerome@etu.unice.fr>
2019-11-07 21:45:39 +0100

    Update Settings.java
    [ITER 2] Noms des zones et plusieurs joueurs

--- src/main/java/game/Settings.java
+++ src/main/java/game/Settings.java
@@ -1,12 +1,16 @@
-package Game;
+package game;
 
 public class Settings {
 	/* STATIC FINAL FIELDS */
-	public static final int START_FIGURINE = 1;
-	public static final int MAX_FIGURINE = 1;
+	public static final int START_FIGURINE = 5;
+	public static final int MAX_FIGURINE = 10;
 	public static final int MAX_CHOICES = 1;
-	public static final int NB_ZONES = 1;
-	public static final int MAX_PLAYERS = 1;
+	public static final int NB_ZONES = 4;
+	public static final int MAX_PLAYERS = 2;
 	public static final int MAX_FIGURINE_IN_ZONE = 7;
 	public static final int MAX_DICE = 6;
+
+	public static String[] ZONE_NAMES = {"Forêt", "Argile", "Pierre", "Or"};
+	public static String[] RESSOURCES_NAMES = {"bois", "argile", "pierre", "or"};
+
 }
