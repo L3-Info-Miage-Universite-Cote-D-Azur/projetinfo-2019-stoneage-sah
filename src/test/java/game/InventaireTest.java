b4922090f837157c5d1cc192970f97e4596694ce
AnthonyS06 <56682383+AnthonyS06@users.noreply.github.com>
2019-11-07 21:44:04 +0100

    Nouveaux test pour l'inventaire mis a jour

--- src/test/java/game/InventaireTest.java
+++ src/test/java/game/InventaireTest.java
@@ -1,33 +1,88 @@
-package Game;
+package game;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class InventaireTest {
-
-	private Inventaire inv;
+public class InventaireTest {
 	
+	private Inventaire inv;
+
 	@Test
 	public void testAddWood () {
 		/* DEPENDENCIES INITIALISATION */
 		
 		/* UNIT TESTS */
-		boolean ret;
+		int ret;
 		this.inv = new Inventaire();
 		
-		ret = this.inv.addWood(-1);
-		assertEquals(0, this.inv.getWood());
-		assertEquals(false, ret);
+		ret = this.inv.addResource(9, 0);
+		assertEquals(3,this.inv.getWood());
+		assertEquals(3,ret);
+		
+		ret = this.inv.addResource(-1, 0);
+		assertEquals(3, this.inv.getWood());
+		assertEquals(-1, ret);
 		
 		
-		ret = this.inv.addWood((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE)/3 + 1);
-		assertEquals(0, this.inv.getWood());
-		assertEquals(false, ret);
+		ret = this.inv.addResource((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE) + 1,0);
+		assertEquals(3, this.inv.getWood());
+		assertEquals(-1, ret);
 		
-		ret = this.inv.addWood((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE)/3);
-		assertEquals((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE)/3, this.inv.getWood());
-		assertEquals(true, ret);
+		ret = this.inv.addResource((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE),0);
+		assertEquals((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE)/3 +3 , this.inv.getWood());
+		assertEquals(17, ret);
+		
+		ret = this.inv.addResource(8, 1);
+		assertEquals(2,this.inv.getClay());
+		assertEquals(2,ret);
+		
+		ret = this.inv.addResource(-1, 1);
+		assertEquals(2, this.inv.getClay());
+		assertEquals(-1, ret);
+		
+		
+		ret = this.inv.addResource((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE) + 1,1);
+		assertEquals(2, this.inv.getClay());
+		assertEquals(-1, ret);
+		
+		ret = this.inv.addResource((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE),1);
+		assertEquals((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE)/4 +2, this.inv.getClay());
+		assertEquals(12, ret);
+		
+		ret = this.inv.addResource(10, 2);
+		assertEquals(2,this.inv.getStone());
+		assertEquals(2,ret);
+		
+		ret = this.inv.addResource(-1, 0);
+		assertEquals(2, this.inv.getStone());
+		assertEquals(-1, ret);
+		
+		
+		ret = this.inv.addResource((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE) + 1,2);
+		assertEquals(2, this.inv.getStone());
+		assertEquals(-1, ret);
+		
+		ret = this.inv.addResource((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE),2);
+		assertEquals((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE)/5 +2, this.inv.getStone());
+		assertEquals(10, ret);
+		
+		ret = this.inv.addResource(12, 3);
+		assertEquals(2,this.inv.getGold());
+		assertEquals(2,ret);
+		
+		ret = this.inv.addResource(-1, 3);
+		assertEquals(2, this.inv.getGold());
+		assertEquals(-1, ret);
+		
+		
+		ret = this.inv.addResource((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE) + 1,3);
+		assertEquals(2, this.inv.getGold());
+		assertEquals(-1, ret);
+		
+		ret = this.inv.addResource((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE),3);
+		assertEquals((Settings.MAX_DICE * Settings.MAX_FIGURINE_IN_ZONE)/6 +2, this.inv.getGold());
+		assertEquals(9, ret);
 	}
 
 }
