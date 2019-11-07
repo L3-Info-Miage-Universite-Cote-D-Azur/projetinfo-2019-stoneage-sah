822457e85256af0d849e22be21615545941fe849
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-07 21:56:47 +0100

    Comment + IA
    ajout des commentaires et de getRandomIA et ce qui s'y rapporte

--- src/main/java/game/Settings.java
+++ src/main/java/game/Settings.java
@@ -1,4 +1,9 @@
 package game;
+import client.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Settings {
 	/* STATIC FINAL FIELDS */
@@ -13,4 +18,31 @@ public class Settings {
 	public static String[] ZONE_NAMES = {"Forêt", "Argile", "Pierre", "Or"};
 	public static String[] RESSOURCES_NAMES = {"bois", "argile", "pierre", "or"};
 
+	public static ArrayList<String> PLAYER_NAME = new ArrayList<String>(Arrays.asList("Robert","Sardoche","Doritos","JeanCharles","Mclito&Carfly","Helico42"));
+	public static IA[] IA_list = new IA[]{new RandomIA()};
+
+	/* STATIC METHODS */
+	
+	/**
+	 * Renvoie un nom au hasard parmis une liste de nom.
+	 * @return String ret, le nom de la personnage pris au hasard. 
+	 */
+	public static String getRandomName ()
+	{
+		Random rand = new Random();
+		int index = rand.nextInt(Settings.PLAYER_NAME.size());
+		String ret = Settings.PLAYER_NAME.get(index);
+		Settings.PLAYER_NAME.remove(index);
+		return ret;
+	}
+
+	/**
+	 * Renvoie une IA (qui suit une strategie) au hasard parmis une liste de strategie d'IA.
+	 * @return IA, avec sa strategie.
+	 */
+	public static IA getRandomIA()
+	{
+		Random rand = new Random();
+		return IA_list[rand.nextInt(IA_list.length)];
+	}
 }
