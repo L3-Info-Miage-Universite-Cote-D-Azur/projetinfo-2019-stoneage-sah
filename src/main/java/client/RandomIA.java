a4e51756998cc33edcc437e1ef3c0e85f1c9993c
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-07 22:02:56 +0100

    Creation randomIA
    Realisé avec Theoricien.
    concerned : #22 et #21

---
+++ src/main/java/client/RandomIA.java
@@ -0,0 +1,31 @@
+package client;
+
+import java.util.Random;
+
+import Game.Settings;
+
+/**
+ * RandomIA correspond a l'IA comportant une strategie basee sur le hasard. 
+ * @author Mentra20
+ *
+ */
+
+public class RandomIA implements IA {
+
+    public int chooseZone () {
+    	// La selection se fait au hasard. 
+        return new Random().nextInt(Settings.NB_ZONES); // RANDOM ZONE
+    }
+    
+    public int chooseNumber (int nbFigurineAvailable) {
+    	if (nbFigurineAvailable <= 0 || nbFigurineAvailable > Settings.MAX_FIGURINE)
+    		return -1;
+    	// La selection se fait au hasard. 
+    	Random r = new Random();
+    	return r.nextInt(nbFigurineAvailable) + 1;
+    }
+
+    public String toString(){
+        return "Random";
+    }
+}
