8788ec402fa0b37f285119e718f838970b926048
Idrash <56682531+Idrash@users.noreply.github.com>
2019-11-07 22:12:19 +0100

    Main class
    déplacement de la class Program qui contient le main

---
+++ src/main/java/Program.java
@@ -0,0 +1,13 @@
+import game.Board;
+
+public class Program {
+	/**
+	 * Cette classe contient le main et lance la partie
+	 */
+
+	
+    public static void main(String[] args) {
+    	Board game = new Board();
+    	game.run();
+    }
+}
