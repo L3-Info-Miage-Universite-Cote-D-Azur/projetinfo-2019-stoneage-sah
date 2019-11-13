730aa10c9ab67929e36304b06b8fa45b648d9e33
iopama <56683098+iopama@users.noreply.github.com>
2019-11-13 21:57:38 +0100

    Changement de Program
    Refonte de Program
    Concerned : #27

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,13 +1,14 @@
-import game.Board;
+import game.Game;
 
 public class Program {
 	/**
 	 * Cette classe contient le main et lance la partie
 	 */
 
-	
     public static void main(String[] args) {
-    	Board game = new Board();
-    	game.run();
+    	//System.setProperty( "file.encoding", "UTF-8" );
+    	System.out.println("\n######## L'AGE DE PIERRE ########\n");
+    	Game game= new Game();
+    	game.gameLoop();
     }
 }
