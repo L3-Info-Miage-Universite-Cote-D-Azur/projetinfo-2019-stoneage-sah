8c7b80ec8a87454e850848305eacbcbfa24d8990
theoricien <maxime.jerome@etu.unice.fr>
2019-11-13 21:48:16 +0100

    Adaptation pour l'iteration 3
    #23 
    La class SETTING représente toujours les paramètres du jeu, les options. Cela permet une meilleur gestion du jeu où l'on peut retrouver toutes nos constantes et quelques fonctions utilitaires.
    Bientôt nous rajouterons une variable contenant une instance de java.util.Random pour ne pas ré-instancier à chaque appel de fonction.

---
+++ src/main/java/game/SETTING.java
@@ -0,0 +1,58 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import client.IA;
+import client.RandomIA;
+
+/**
+ * La classe represente les parametres immutables du jeu.  
+ */
+
+public class SETTING
+{
+	/* STATIC FINAL FIELDS */
+	// Nombre de figurine au depart pour chaque joueur
+	public static final int START_FIGURINE = 5;
+	// Nombre de figurine maximale pour chaque joueur
+	public static final int MAX_FIGURINE = 10;
+	// Nombre de joueur dans la partie
+	public static final int MAX_PLAYER = 2;
+	// Nombre de place dans les zones de ressources. 
+	public static final int MAX_ZONERESSOURCE_SPACE = 7;
+    
+	// Liste des noms de joueurs. 
+	public static ArrayList<String> PLAYER_NAME = new ArrayList<String>(Arrays.asList("Robert","Sardoche","Doritos","JeanCharles","Mclito&Carfly","Helico42"));
+    
+	public static IA[] IA_list = new IA[]{new RandomIA()};
+    
+	/* STATIC METHODS */
+	
+	/**
+	 * Renvoie un nom au hasard parmis une liste de nom.
+	 * @return String ret, le nom de la personnage pris au hasard. 
+	 */
+	public static String getRandomName ()
+	{
+		Random rand = new Random();
+		int index = rand.nextInt(SETTING.PLAYER_NAME.size());
+		
+		String ret = SETTING.PLAYER_NAME.get(index);
+		SETTING.PLAYER_NAME.remove(index);
+		
+		return ret;
+	}
+	
+	/**
+	 * Renvoie une IA parmi celles présentes aléatoirement
+	 * @return Renvoie une instance implémentant l'interface IA
+	 */
+	public static IA getRandomIA () 
+	{
+		Random rand = new Random();
+		return IA_list[rand.nextInt(IA_list.length)];
+	}
+    
+}
