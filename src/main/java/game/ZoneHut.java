4bce411e39a35c57c296933574e467affeabe7f0
iopama <56683098+iopama@users.noreply.github.com>
2019-11-14 18:37:40 +0100

    Ajout du javadoc pour la Cabane

--- src/main/java/game/ZoneHut.java
+++ src/main/java/game/ZoneHut.java
@@ -6,16 +6,17 @@ package game;
  * La classe represente les zones de ressource du jeu. 
  */
 
+
 class ZoneHut implements Zone{
     /* FIELDS */
 	private int availableSpace;//Les places disponible dans la zone actuellement.
-	//Dictionnaire qui contient pour un joueur, le nombre de figurine qu'il a dans la zone.
-	private Player occupated;
-	private int figurineInZone;
+	private Player occupated;//Le joueur present dans la zone
+	private int figurineInZone;//Dictionnaire qui contient pour un joueur, le nombre de figurine qu'il a dans la zone.
 	private String name; //Le nom de la zone
 	
 	/* FINAL FIELDS */
-    //Probablement inutile dans cette conception private final int totalAvailableSpace;//La place totale disponible dans la zone. 
+    //Probablement inutile dans cette conception private final int totalAvailableSpace;
+	//La place totale disponible dans la zone. 
     
     ZoneHut(String name){
         this.name=name;
@@ -34,7 +35,8 @@ class ZoneHut implements Zone{
     //Figurine
     /**
      * placeFigurine(int, Player) place number figurine appartenant a player dans la zone. 
-     * @param number : le nombre de figurine a mettre dans la zone, player : le joueur qui les mets. 
+     * @param number : le nombre de figurine a mettre dans la zone.
+     * @param player : le joueur qui les mets. 
      */ 
     public void placeFigurine(int number,Player player){
         availableSpace-=number;
@@ -43,8 +45,9 @@ class ZoneHut implements Zone{
     }
     
     /**
-     * 
-     * @return
+     * Decide si le joueur peut choisir cette zone.
+     * @param currentplayerFigurine : le nombre de figurine restant au joueur.
+     * @return l'espace restante dans la zone si superieur a 0
      */
     public boolean ableToChooseZone(int currentPlayerFigurine){
         return (availableSpace>0);
@@ -60,16 +63,22 @@ class ZoneHut implements Zone{
     }
     
     /**
-     * Retourne le nombre de figurines que player a dans la zone. 
-     * @param player: le joueur dont on veut savoir le nombre de figurines dans la zone. 
+     * Retourne le nombre de figurines dans la zone. 
+     * @param player: le joueur qui peut placer ses figurines.
+     * @return 0 s'il n'y a pas de figurine dans la zone ou si le joueur est différent de celui qui occupe celle-ci
+     * @return figurineZone s'il y a de la place dans la zone 
      */ 
     public int howManyPlayerFigurine(Player player){
         if(figurineInZone == 0 || occupated != player){
             return 0;
         }
         return figurineInZone;
-    } 
+    }
     
+    /**
+     * Retourne le nombre de figurines que player a dans la zone. 
+     * @param player: le joueur dont on veut savoir le nombre de figurines dans la zone. 
+     */ 
     public void playerRecoveryFigurine(Player player) {
     	int number = howManyPlayerFigurine(player);
         removePlayerFigurine(player);
@@ -78,8 +87,13 @@ class ZoneHut implements Zone{
         if(ok) { System.out.println("Le joueur "+ player.getName()+" a maintenant "+ player.getMaxFigurine()+" figurine");}
         else { System.out.println("Le joueur "+ player.getName()+" a deja le maximum de figurine");}
     }
-    	
+    
+    /**
+     * Donne le nombre de figurine minimum necessaire pour occuper la zone.
+     * @return 2.
+     */
     public int getMinimalFigurineRequierement() {
     	return 2;
     }
+}
 }
