2863db3342c353b25de810614fe2e8faa2d920f5
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-14 14:58:22 +0100

    Changement du code.
    Concerned : #27

--- src/main/java/client/IA.java
+++ src/main/java/client/IA.java
@@ -1,25 +1,28 @@
 package client;
 
 /**
- * Interface de l'IA comportant les fonctions utiles.
+ * Interface pour l'IA du jeu. 
  * @author Mentra20
  *
  */
 
-public interface IA {
+public interface IA{
 	
 	/**
-	 * choozeZone choisie une zone selon la strategie de l'IA. 
-	 * @return int :zone la zone choisie.
+	 * chooseZone retourne l'indice de la zone choisie par l'IA . 
+	 * @param zoneAvailableSpace le tableau avec l'espace disponible de chaque zone. 
+	 * @param zoneName le tableau des noms des zones.
+	 * @return l'indice de la zone
 	 */
-    public int chooseZone ();
-    
-    /**
-	 * choozeNumber(int) choisi le nombre de figurines a mettre dans la zone selon la strategie de l'IA. 
-	 * @return int : le nombre de figurine choisies. 
+	public int chooseZone(int[] zoneAvailableSpace,String[] zoneName);
+	
+	/**
+	 * chooseNumber retourne le nombre de figurines choisie par l'IA. 
+	 * @param min et max, le nombre minimum et maximum entre lesquels l'IA doit choisir. 
+	 * @return l'indice de la zone
 	 */
-    public int chooseNumber (int nbFigurineAvailable);
+	public int chooseNumber(int min,int max);
 
-    @Override
-    public String toString();
+	@Override
+	public String toString();
 }
