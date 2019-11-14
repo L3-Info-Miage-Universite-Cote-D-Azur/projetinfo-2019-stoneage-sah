67e9e9f4f4b6509958ea7cc30e4912edd2df6948
iopama <56683098+iopama@users.noreply.github.com>
2019-11-14 16:12:48 +0100

    Petit changement de Dice

--- src/main/java/game/Dice.java
+++ src/main/java/game/Dice.java
@@ -4,7 +4,7 @@ package game;
 import java.util.Random;
 
 /**
- * La classe Dice represente le dé non truque du plateau.  
+ * La classe Dice represente le dÃ© non truque du plateau.  
  */
  
 class Dice{
@@ -14,12 +14,11 @@ class Dice{
      * @param number: le nombre de fois qu'il faut lancer le de. 
      * @return le tableau d'entier contenant les resultats du lancer de de number fois. 
      */ 
-    public static int[] rollDice(int number){
+      public static int[] rollDice(int number){
         int[] tab = new int[number];
-        Random r = new Random();
         
         for(int i=0; i<number; i++) {
-        	tab[i] = r.nextInt(6)+1;
+        	tab[i] = Settings.RAND.nextInt(6)+1;
         }
         return tab;
     }
