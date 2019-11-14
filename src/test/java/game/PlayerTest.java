61be73cca2347844301e0c70af4a270fed8ad8d1
theoricien <maxime.jerome@etu.unice.fr>
2019-11-14 15:51:37 +0100

    Adaptation pour l'itération 3
    #27

--- src/test/java/game/PlayerTest.java
+++ src/test/java/game/PlayerTest.java
@@ -6,52 +6,63 @@ import org.junit.jupiter.api.Test;
 
 import client.RandomIA;
 
-public class PlayerTest {
+public class PlayerTest
+{
     private Player player;
+
     @Test
-    public void testAddFigurineInZone() {
+    public void testIncreaseMaxFigurine() 
+    {
         this.player = new Player(Settings.getRandomName(), new RandomIA());
-        
-        // Zone Inexistante
-        this.player.addFigurineInZone(Settings.NB_ZONES,1);
-        assertEquals(Settings.START_FIGURINE,this.player.getFigurine());
-        this.player.addFigurineInZone(-2,1);
-        assertEquals(Settings.START_FIGURINE,this.player.getFigurine());
-        
-        this.player = new Player(Settings.getRandomName(), new RandomIA());
-        // Figurine hors de portee
-        this.player.addFigurineInZone(0,Settings.START_FIGURINE + 1);
-        assertEquals(Settings.START_FIGURINE,this.player.getFigurine());
-        this.player = new Player(Settings.getRandomName(), new RandomIA());
-        this.player.addFigurineInZone(0,-2);
-        assertEquals(Settings.START_FIGURINE,this.player.getFigurine());
-        
-        // Expected in program
-        this.player.addFigurineInZone(0,1);
-        System.out.print(this.player.getFigurine());
-        assertEquals(Settings.START_FIGURINE - 1,this.player.getFigurine());
+        // INIT TEST
+        assertEquals(this.player.getMaxFigurine(), Settings.START_FIGURINE);
+
+        // TEST
+        assertEquals(this.player.increaseMaxFigurine(), true);
+        assertEquals(this.player.getCurrentFigurine(), Settings.START_FIGURINE + 1);
+
+        // FILL IT
+        for (int i = 0; i < Settings.MAX_FIGURINE - this.player.getMaxFigurine(); i++)
+        {
+            assertEquals(this.player.increaseMaxFigurine, true);
+        }
+
+        // ERROR TEST
+        assertEquals(this.player.increaseMaxFigurine(), false);
+        // IS IT FILLED
+        assertEquals(this.player.getMaxFigurine(), Settings.MAX_FIGURINE);
+        assertEquals(this.player.getCurrentFigurine(), Settings.MAX_FIGURINE);
     }
     
     @Test
-    public void testSubFigurineInZone() {
+    public void testAbleToPlaceFigurine () 
+    {
         this.player = new Player(Settings.getRandomName(), new RandomIA());
-        // Rien a recuperer
-        this.player.subFigurineInZone(0);
-        assertEquals(Settings.START_FIGURINE,this.player.getFigurine());
         
-        
-        this.player.addFigurineInZone(0,1);
-        // Zone Inexistante
-        this.player.subFigurineInZone(Settings.NB_ZONES);
-        assertEquals(Settings.START_FIGURINE - 1,this.player.getFigurine());
-        
-        this.player.subFigurineInZone(-5);
-        assertEquals(Settings.START_FIGURINE - 1,this.player.getFigurine());
-        
-        // Expected in program
-        this.player.subFigurineInZone(0);
-        assertEquals(Settings.START_FIGURINE,this.player.getFigurine());
-        
+        assertEquals(this.player.ableToPlaceFigurine(0), true);
+
+        this.player.setHadPlace(0, true);
+
+        assertEquals(this.player.ableToPlaceFigurine(0), false);
+        assertEquals(this.player.ableToPlaceFigurine(1), true);
+        assertEquals(this.player.ableToPlaceFigurine(Settings.NB_ZONES - 1), true);
+
+        for (int i = 0; i < Settings.NB_ZONES; i++)
+        {
+            this.player.setHadPlace(i, true);
+        }
+
+        for (int i = 0; i < Settings.NB_ZONES; i++)
+        {
+            assertEquals(this.player.ableToPlaceFigurine(i), false);
+        }
+
+        this.player.resetHadPlaced();
+
+        for (int i = 0; i < Settings.NB_ZONES; i++)
+        {
+            assertEquals(this.player.ableToPlaceFigurine(i), true);
+        }
     }
 
 }
