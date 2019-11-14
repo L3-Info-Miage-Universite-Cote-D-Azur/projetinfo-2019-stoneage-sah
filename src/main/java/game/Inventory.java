50cd87c9aa40ba9180fd8ed93281f6df053f44e0
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-14 20:26:12 +0100

    ajout de fonctions
    Fait avec Anthony Santi 
    Concerned : #26

--- src/main/java/game/Inventory.java
+++ src/main/java/game/Inventory.java
@@ -1,9 +1,5 @@
 package game;
 
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Set;
-
 /**
  * La classe Inventory gere l'inventaire du joueur.
  */
@@ -13,20 +9,15 @@ public class Inventory {
 	
 	/* FIELDS */
 	// Les ressources que peut avoir le joueur. 
-	private Hashtable<Ressource,Integer> ressources;
+	private int[] ressources;
 	
 	/* CONSTRUCTOR */
 	public Inventory () {
-		ressources = new Hashtable<Ressource,Integer>();
+		ressources = new int[Settings.NB_RESSOURCES];
 	}
 	
 	//GETTERS
-	public int getRessource(Ressource ressource){
-	    if(ressources.get(ressource) == null){
-	        return 0;
-	    }
-	    return ressources.get(ressource);
-	}
+	public int getRessource(Ressource ressource){ return ressources[ressource.getIndex()] ;}
 	
 	//ADDERS
 	/**
@@ -34,7 +25,7 @@ public class Inventory {
 	 * @param ressource la ressource et number le nombre de ressources gagnee. 
 	 */
 	public void addRessource(Ressource ressource,int number){
-	    ressources.put(ressource,getRessource(ressource)+number);
+		ressources[ressource.getIndex()]+=number;
 	}
 	
 	//SUBSTRACTORS
@@ -43,7 +34,7 @@ public class Inventory {
 	 * @param ressource la ressource et number le nombre de ressources retiree. 
 	 */
 	public void subRessource(Ressource ressource,int number) {
-	    ressources.put(ressource,getRessource(ressource)-number);
+		ressources[ressource.getIndex()]-=number;
 	}
 	
 	/**
@@ -54,72 +45,21 @@ public class Inventory {
 	 * 
 	 */
 	public boolean ableToSubRessource(Ressource ressource,int number) {
-		if(ressources.get(ressource) < number) return false;
+		if(ressources[ressource.getIndex()] < number) return false;
 		return true;
 	}
 	
 	/**
-	 * Renvoie la liste des ressource premiere (sans outil ou Field)
-	 * @return liste des ressource premiere 
+	 * Ressource utilisable total 
+	 * (utiliser pour voir si le joueur peut nourire toutes les figurine quand il a plus de nouriture)
+	 * @return somme des ressource usable pour remplacer la nouriture
 	 */
-	public String[] toArrayRessource() {
-		Set<Ressource> setRessources = ressources.keySet();
-		String[] ressource = new String[setRessources.size()];
-
-		Iterator<Ressource> it;
-		it=setRessources.iterator(); // on cree un iterator sur les clés de ton hashmap
-		int i=0;
-		while(it.hasNext())
-		{
-			Ressource r = (Ressource) it.next();
-			if(r!=Ressource.FIELD) {ressource[i]=r.toString();}
-			i+=1;
+	public int availableResourceToFeed() {
+		int sum=0;
+		for(int i=0; i<=3;i++) {
+			sum+=ressources[i];
 		}
-		return ressource;
-		
-	}
-	
-	/**
-	 * Renvoie la liste des ressource premiere (sans outil ou Field)
-	 * elle fonctionne avec toArrayRessource pour avoir a quel ressource l'indice du tableau correspond.
-	 * @return nombre de ressource.
-	 */
-	public int[] toArrayNumber() {
-		Set<Ressource> setRessources = ressources.keySet();
-		int[] number = new int[setRessources.size()];
-
-		Iterator<Ressource> it;
-		it=setRessources.iterator(); // on cree un iterator sur les clés de ton hashmap
-		int i=0;
-		while(it.hasNext())
-		{
-			Ressource r = (Ressource) it.next();
-			if(r!=Ressource.FIELD) {number[i]=ressources.get(r);}
-			i+=1;
-		}
-		return number;
-		
-	}
-	
-	/**
-	 * Renvoie la liste des ressource premiere (sans outil ou Field)
-	 * elle fonctionne avec toArrayRessource et toArrayNumber pour avoir a quel ressource l'indice du tableau correspond.
-	 * @return nombre de ressource.
-	 */
-	public Ressource[] toArrayRessourceEnum() {
-		Set<Ressource> setRessources = ressources.keySet();
-		Ressource[] ressource = new Ressource[setRessources.size()];
-
-		Iterator<Ressource> it;
-		it=setRessources.iterator(); // on cree un iterator sur les clés de ton hashmap
-		int i=0;
-		while(it.hasNext())
-		{
-			Ressource r = (Ressource) it.next();
-			if(r!=Ressource.FIELD) {ressource[i]=r;}
-			i+=1;
-		}
-		return ressource;
-		
+		return sum;
 	}
 }
+
