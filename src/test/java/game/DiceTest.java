9f7ff2022966c0aac99f4a5f01a3c634e09fc538
iopama <56683098+iopama@users.noreply.github.com>
2019-11-14 21:42:24 +0100

    Test de la classe Dice
    Concerned : #38

--- src/test/java/game/DiceTest.java
+++ src/test/java/game/DiceTest.java
@@ -3,16 +3,18 @@ package game;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import game.Dice;
 
 public class DiceTest {
 
 	@Test
-	public void testRoll() {
-		for (int i =0; i<100; i++) {
-			int dice = Dice.roll();
-			assertEquals(true, dice <= 6 && dice >=1);
+	//* Test pour verifier si le lancers de de sont bien entre 1 et 6 dans tout le tableau, et si le tableau est de la bonne longueur.
+	public void testRollDice() {
+		int number=6;
+		int[] tab = Dice.rollDice(number);
+		for (int i=0;i<number;i++) {
+			assertEquals(true, tab[i] <= 6 && tab[i] >=1);
 		}
+		assertEquals(true,tab.length==number);
 	}
 
 }
