83fd287a7839ba9bd0ced4b8d726a45047e041b1
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-15 19:16:29 +0100

    Correction Javadoc

--- src/main/java/game/FigurineManagement.java
+++ src/main/java/game/FigurineManagement.java
@@ -8,8 +8,10 @@ class FigurineManagement{
     
     /**
      * ableToPlaceFigurine() utilise les ableToPlaceFigurine() de zone et de player pour savoir si cela est possible. 
-     * @param number : le nombre de figurine a placer, player : le joueur concerne, zone : la zone concernee.
-     * @return true si possible de placer, false sinon
+     * @param zone : la zone concernee.
+     * @param player : le joueur concerne. 
+     * @param number : le nombre de figurine a placer. 
+     * @return true si possible de placer, false sinon.
      */ 
     public static boolean ableToChooseZone(Zone zone,Player player,int indexZone){
     	//le indexZone == 4 est l'index de la chasse ou l'on peut posser tout le temps
@@ -18,7 +20,10 @@ class FigurineManagement{
     
     /**
      * placeFigurine fait appel au placeFigurine de player et de zone puis affiche un message bilan. 
-     * @param number : le nombre de figurine a placer, player : le joueur concerne, zone : la zone concernee.
+     * @param zone : la zone concernee.
+     * @param player : le joueur concerne, zone : la zone concernee.
+     * @param number : le nombre de figurine a placer.
+     * @param indexZone : l'index de la zone correspondante.
      */ 
     public static void placeFigurine(Zone zone, Player player,int number,int indexZone){
         zone.placeFigurine(number, player); 
@@ -30,9 +35,9 @@ class FigurineManagement{
     //affichage
     /**
      * Affichage textuel du joueur qui place dans la zone un nombre de figurine
-     * @param zone zone a affichage
-     * @param player player a afficher
-     * @param number nombre a qui a etait placer
+     * @param zone : zone a afficher
+     * @param player : player a afficher
+     * @param number : nombre a qui a etait placer
      * @return phrase qui resume l'action
      */
     public static String stringPlaceFigurine(Zone zone, Player player, int number){
@@ -43,7 +48,8 @@ class FigurineManagement{
     //phase de recuperation des figurine
     /**
      * permet a player de recupere ses figurines dans zone et renvoie le nombre de ressource gagnee dedans. 
-     * @param zone : la zone concern�e, player : le joueur qui recupere les figurines. 
+     * @param zone : la zone concernee. 
+     * @param player : le joueur qui recupere les figurines. 
      */ 
     public static void recoveryFigurineInZone(Zone zone, Player player){
     	zone.playerRecoveryFigurine(player);
