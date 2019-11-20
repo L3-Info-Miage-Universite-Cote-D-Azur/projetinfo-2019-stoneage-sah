40b482d7d367f6c84fdcdfa6c72d08d71d941783
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-20 20:40:48 +0100

    Ajout javadoc pour la derniere fonction

---
+++ src/main/java/game/RandomIA.java
@@ -0,0 +1,71 @@
+package client;
+
+import java.util.Random;
+
+
+/**
+ * RandomIA correspond a l'IA comportant une strategie basee sur le hasard. 
+ * @author Mentra20
+ *
+ */
+
+public class RandomIA implements IA {
+	
+	//Random de l'IA (elle ne peux pas acceder a Settings).
+	private Random rand = new Random();
+	
+	/**
+	 * chooseZone retourne l'indice de la zone choisie par l'IA (ici au hasard). 
+	 * @param zoneAvailableSpace le tableau avec l'espace disponible de chaque zone. 
+	 * @param zoneName le tableau des noms des zones.
+	 * @return l'indice de la zone
+	 */
+	public int chooseZone (int[] zoneAvailableSpace,String[] zoneName) {
+		return rand.nextInt(zoneAvailableSpace.length);
+	}
+
+	/**
+	 * chooseNumber retourne le nombre de figurines choisie par l'IA (ici au hasard). 
+	 * @param min et max, le nombre minimum et maximum entre lesquels l'IA doit choisir. 
+	 * @return l'indice de la zone
+	 */
+	public int chooseNumber (int min,int max) {
+		return rand.nextInt(max-min+1)+min;
+	}
+	
+	/**
+	 * chooseNumber retourne le nombre de ressource choisie par l'IA au hasard. 
+	 * @param Le nombre de figurines a nourrir
+	 * @param Un tableau contenant le nombre de ressource que l'IA possede par index.
+	 * @param Un tableau contenant le nom des ressources que l'IA possede par index.
+	 * @return l'indice de la zone
+	 */
+	public int[] chooseRessource(int figurinesToFeed, int[] RessourceNumber, String[] ressourceName) {
+        int ressource;
+        int index;
+        do {
+            index = rand.nextInt(RessourceNumber.length);
+            ressource = RessourceNumber[index];
+        }while(ressource==0);
+        
+        int nb = rand.nextInt(Math.min(ressource, figurinesToFeed))+1;
+        
+        int[] res = new int[] {index,nb};
+        return res;
+    }
+
+	/**
+	 * useRessourceToFeed renvoie true ou false selon le choix de l'IA pour nourrir 
+	 * ses figurines avec des ressources. Le choix se fait au hasard.  
+	 * @param RessourceNumber Le tableau du nombre de ressource possedee par le joueur. 
+	 * @return Booleen : true si le joueur veut utiliser ses ressources pour nourrir, false sinon.
+	 */
+	public boolean useRessourceToFeed(int[] RessourceNumber) {
+		return rand.nextInt(2)==1;
+	}
+	
+	public String toString(){
+		return "Random";
+	}
+}
+
