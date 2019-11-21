2718a3331a7f1c6e5c979bcc66f3217fe2c87119
theoricien <maxime.jerome@etu.unice.fr>
2019-11-21 16:02:57 +0100

    Ajout des fonctions pour les tuiles batiments #52
    #52

--- src/main/java/game/Settings.java
+++ src/main/java/game/Settings.java
@@ -28,10 +28,17 @@ public class Settings
 	public static final int MAX_ZONEHUT_SPACE = 2;
 	// Nombre d'espace dans le champ
 	public static final int MAX_ZONEFIELD_SPACE = 1;
-	//Nombre de ressource present dans le jeu:
+	// Nombre de ressource present dans le jeu:
 	public static final int NB_RESSOURCES = 6;
 	// Aleatoire globale
 	public static Random RAND = new Random();
+	// Nombre de ressources au maximum dans BuildingRessourceChoosed
+	public static int NB_MAX_RESSOURCE_BUILDING = 7;
+	// Nombre de cartes dans un deck
+	public static int NB_BUILDING_CARD_IN_DECK = 7;
+	
+	// Toutes les cartes tuiles batiment du jeu
+	public static ArrayList<Building> BUILDINGS = new ArrayList<Building>(Arrays.asList(new BuildingRessourceImposed(10, Ressource.WOOD, Ressource.WOOD, Ressource.CLAY)));
 		
 	// Liste des noms de joueurs. 
 	public static ArrayList<String> PLAYER_NAME = new ArrayList<String>(Arrays.asList("Robert","Sardoche","Doritos","JeanCharles","Mclito&Carfly","Helico42"));
@@ -55,12 +62,29 @@ public class Settings
 	}
 	
 	/**
-	 * Renvoie une IA parmi celles presentes aléatoirement
+	 * Renvoie une IA parmi celles presentes alÃ©atoirement
 	 * @return Renvoie une instance implementant l'interface IA
 	 */
 	public static IA getRandomIA () 
 	{
 		return IA_list[Settings.RAND.nextInt(IA_list.length)];
+	}
+	
+	/**
+	 * Renvoie un deck de cartes de tuiles batiments
+	 * @return un deck de cartes de tuiles batiments
+	 */
+	public static ArrayList<Building> getRandomDeck()
+	{
+		ArrayList<Building> buildings = new ArrayList<Building>(Settings.NB_BUILDING_CARD_IN_DECK);
+		for (int i = 0; i < Settings.NB_BUILDING_CARD_IN_DECK; i++)
+		{
+			buildings.add(Settings.BUILDINGS.get(0));
+			Settings.BUILDINGS.remove(0);
+			if (Settings.BUILDINGS.isEmpty())
+				return buildings;
+		}
+		return buildings;
 	}
     
 }
