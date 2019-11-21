15fba78dbc812526d1a2171fc6f92500755175b0
theoricien <maxime.jerome@etu.unice.fr>
2019-11-21 21:13:44 +0100

    Test GameUtility #71
    #71

--- src/test/java/game/GameUtilityTest.java
+++ src/test/java/game/GameUtilityTest.java
@@ -16,7 +16,7 @@ public class GameUtilityTest {
 		Player[] test = GameUtility.initPlayer(); 
 		
 		//Test que la longueur est bonne
-		assertEquals(2, test.length);
+		assertEquals(Settings.MAX_PLAYER, test.length);
 		
 		//Test que les prenoms sont correctes
 		assertEquals(true, liste.contains(test[0].getName()));
@@ -28,63 +28,82 @@ public class GameUtilityTest {
 	}
 	
 	@Test
-	public void testInitZone() {
-		//On test si les zone creer sont correcte. 
-		Zone[] test = GameUtility.initZone();
-		
-		assertEquals("Foret", test[0].getName());
-		assertEquals(Settings.MAX_ZONERESSOURCE_SPACE, test[0].getAvailableSpace());
-		assertEquals(1, test[0].getMinimalFigurineRequierement());
-
-		assertEquals("Glaisiere", test[1].getName());
-		assertEquals(Settings.MAX_ZONERESSOURCE_SPACE, test[1].getAvailableSpace());
-		assertEquals(1, test[1].getMinimalFigurineRequierement());
-
-		assertEquals("Carriere", test[2].getName());
-		assertEquals(Settings.MAX_ZONERESSOURCE_SPACE, test[2].getAvailableSpace());
-		assertEquals(1, test[2].getMinimalFigurineRequierement());
-		
-		assertEquals("Riviere", test[3].getName());
-		assertEquals(Settings.MAX_ZONERESSOURCE_SPACE, test[3].getAvailableSpace());
-		assertEquals(1, test[3].getMinimalFigurineRequierement());
-		
-		assertEquals("Chasse", test[4].getName());
-		assertEquals(Settings.MAX_PLAYER * Settings.MAX_FIGURINE, test[4].getAvailableSpace());
-		assertEquals(1, test[4].getMinimalFigurineRequierement());
-		
-		assertEquals("Champs", test[5].getName());
-		assertEquals(Settings.MAX_ZONEFIELD_SPACE, test[5].getAvailableSpace());
-		assertEquals(1, test[5].getMinimalFigurineRequierement());
-		
-		assertEquals("Cabane de reproduction", test[6].getName());
-		assertEquals(Settings.MAX_ZONEHUT_SPACE, test[6].getAvailableSpace());
-		assertEquals(2, test[6].getMinimalFigurineRequierement());
-		
-	}
-	
-	@Test
 	public void testZoneChoose() {
 		Player player = new Player("Jean", new RandomIA());
-		Zone[] zone = GameUtility.initZone();
+		CarteCivilisationManager cardManager;
+		Zone[] zones = new Zone[Settings.NB_ZONES];
+		ZoneCarteCivilisation[] zoneCarteCivilisation = new ZoneCarteCivilisation[4];
+
+		//Les zones du jeu. 
+		zones[0] = new ZoneRessource("Foret",Ressource.WOOD,Settings.MAX_ZONERESSOURCE_SPACE);
+		zones[1] = new ZoneRessource("Glaisiere",Ressource.CLAY,Settings.MAX_ZONERESSOURCE_SPACE);
+		zones[2] = new ZoneRessource("Carriere",Ressource.STONE,Settings.MAX_ZONERESSOURCE_SPACE);
+		zones[3] = new ZoneRessource("Riviere",Ressource.GOLD,Settings.MAX_ZONERESSOURCE_SPACE);
+		//La zone de chasse a : nombre de joueur x le nombre de figurines maximum d'espace. 
+		zones[4] = new ZoneRessource("Chasse", Ressource.FOOD,Settings.MAX_PLAYER * Settings.MAX_FIGURINE);
+		zones[5] = new ZoneField("Champs", Ressource.FIELD);
+		zones[6] = new ZoneHut("Cabane de reproduction");
+		zones[7] = new ZoneTool("Le Fabricant D'outils");
 		
-		int test = GameUtility.zoneChoose(player, zone);
+		//gestion des carte civilisation
+		ZoneCarteCivilisation zcc;
+		for(int i=0;i<4;i++) {
+			zcc=new ZoneCarteCivilisation("Carte Civilisation: "+ (i+1), i+1);
+			zoneCarteCivilisation[i]=zcc;
+			zones[8+i]=zcc;
+		}
+		cardManager = new CarteCivilisationManager(zoneCarteCivilisation);
+		
+		zones[12] = new ZoneBuilding();
+		zones[13] = new ZoneBuilding();
+		zones[14] = new ZoneBuilding();
+		zones[15] = new ZoneBuilding();
+		
+		int test = GameUtility.zoneChoose(player, zones);
 		
 		//Test pour verifier la validite du return
 		assertEquals(true, test >= 0 );
-		assertEquals(true, test < zone.length );
-		assertEquals(true, zone[test].getAvailableSpace() > 0 );
+		assertEquals(true, test < zones.length );
+		assertEquals(true, zones[test].getAvailableSpace() > 0 );
 	}
 	
 	@Test
 	public void testNumberChoose() {
 		Player player = new Player("Jean", new RandomIA());
-		Zone[] zone = GameUtility.initZone();
+		CarteCivilisationManager cardManager;
+		Zone[] zones = new Zone[Settings.NB_ZONES];
+		ZoneCarteCivilisation[] zoneCarteCivilisation = new ZoneCarteCivilisation[4];
+
+		//Les zones du jeu. 
+		zones[0] = new ZoneRessource("Foret",Ressource.WOOD,Settings.MAX_ZONERESSOURCE_SPACE);
+		zones[1] = new ZoneRessource("Glaisiere",Ressource.CLAY,Settings.MAX_ZONERESSOURCE_SPACE);
+		zones[2] = new ZoneRessource("Carriere",Ressource.STONE,Settings.MAX_ZONERESSOURCE_SPACE);
+		zones[3] = new ZoneRessource("Riviere",Ressource.GOLD,Settings.MAX_ZONERESSOURCE_SPACE);
+		//La zone de chasse a : nombre de joueur x le nombre de figurines maximum d'espace. 
+		zones[4] = new ZoneRessource("Chasse", Ressource.FOOD,Settings.MAX_PLAYER * Settings.MAX_FIGURINE);
+		zones[5] = new ZoneField("Champs", Ressource.FIELD);
+		zones[6] = new ZoneHut("Cabane de reproduction");
+		zones[7] = new ZoneTool("Le Fabricant D'outils");
 		
-		for(int i = 0; i < zone.length; i++) {
-			int max = Math.min(player.getCurrentFigurine(),zone[i].getAvailableSpace());
-			int min = zone[i].getMinimalFigurineRequierement();
+		//gestion des carte civilisation
+		ZoneCarteCivilisation zcc;
+		for(int i=0;i<4;i++) {
+			zcc=new ZoneCarteCivilisation("Carte Civilisation: "+ (i+1), i+1);
+			zoneCarteCivilisation[i]=zcc;
+			zones[8+i]=zcc;
+		}
+		cardManager = new CarteCivilisationManager(zoneCarteCivilisation);
+		
+		zones[12] = new ZoneBuilding();
+		zones[13] = new ZoneBuilding();
+		zones[14] = new ZoneBuilding();
+		zones[15] = new ZoneBuilding();
+		
+		for(int i = 0; i < zones.length; i++) {
+			int max = Math.min(player.getCurrentFigurine(),zones[i].getAvailableSpace());
+			int min = zones[i].getMinimalFigurineRequierement();
 			
-			int test = GameUtility.numberChoose(player, zone[i]);
+			int test = GameUtility.numberChoose(player, zones[i]);
 			
 			//Test pour verifier la validite du return
 			assertEquals(true, test >= min && test <= max);
