fa718c5d182ad2a5b7917ab35e10d497150823ee
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-21 22:53:42 +0100

    Ajout du test de la classe

---
+++ src/test/java/game/CarteCivilisationManagerTest.java
@@ -0,0 +1,45 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class CarteCivilisationManagerTest {
+	
+	
+	@Test
+	public void testInitDeck(){
+		ZoneCarteCivilisation[] zcc = new ZoneCarteCivilisation[4];
+
+		for(int i=0;i<4;i++) {
+			zcc[i]=new ZoneCarteCivilisation("Carte Civilisation: "+ (i+1), i+1);
+		}
+		
+		CarteCivilisationManager ccm = new CarteCivilisationManager(zcc);
+		
+		ccm.initDeck();
+		//Test de la longueur
+		assertEquals(true,ccm.getDeck().size() == 20);
+	}
+	
+	@Test
+	public void testGetRandomCivilisationCard() {
+		ZoneCarteCivilisation[] zcc = new ZoneCarteCivilisation[4];
+		for(int i=0;i<4;i++) {
+			zcc[i]=new ZoneCarteCivilisation("Carte Civilisation: "+ (i+1), i+1);
+		}
+		CarteCivilisationManager ccm = new CarteCivilisationManager(zcc);
+		
+		ccm.initDeck();
+		ArrayList<CarteCivilisation> cloneDeck = new ArrayList<CarteCivilisation>(ccm.getDeck());
+		
+		for(int i = 0; i<20; i++) {
+			CarteCivilisation cv = ccm.getRandomCivilisationCard();
+			assertEquals(false, ccm.getDeck().contains(cv));
+			assertEquals(true, cloneDeck.contains(cv));
+		}
+		assertEquals(true, ccm.getDeck().size() == 0);
+	}
+}
