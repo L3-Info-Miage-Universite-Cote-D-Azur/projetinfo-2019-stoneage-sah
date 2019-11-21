77959378f6ffc9eaff6212b09a2abcdf9e674ae9
Idrash <56682531+Idrash@users.noreply.github.com>
2019-11-21 22:35:45 +0100

    Choix du nombre de joueur #42

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -8,7 +8,26 @@ public class Program {
     public static void main(String[] args) {
     	//System.setProperty( "file.encoding", "UTF-8" );
     	System.out.println("\n######## L'AGE DE PIERRE ########\n");
-    	Game game= new Game();
+    	int defaultNumber = 4;
+    	int numberPlayer;
+    	if(args.length==0) { numberPlayer=defaultNumber; }
+    	else{
+    		try{
+    			numberPlayer = Integer.parseInt(args[0]);
+    		}catch(Exception e) {
+    			System.out.println("Choix de joueur invalide : "+e+" il est donc automatiquement initialiser a 4");
+    			numberPlayer = defaultNumber;
+    		}
+    	}
+    	Game game;
+    	if(numberPlayer>1 && numberPlayer<=4) {
+    		game= new Game(numberPlayer);
+    	}
+    	else { 
+    		System.out.println("Choix de joueur invalide il est donc automatiquement initialiser a 4");
+    		game= new Game(numberPlayer);
+			numberPlayer = defaultNumber;
+    	}
     	game.gameLoop();
     }
 }
