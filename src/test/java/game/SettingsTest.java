f2c34627328ea1b609352fd8512c80fd229ba2f7
theoricien <maxime.jerome@etu.unice.fr>
2019-11-21 20:44:51 +0100

    Test Settings #67
    #67

--- src/test/java/game/SettingsTest.java
+++ src/test/java/game/SettingsTest.java
@@ -3,6 +3,7 @@ package game;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +30,54 @@ public class SettingsTest {
 		assertEquals(true,Settings.IA_list.length == 1);
 		assertEquals(false,null == Settings.getRandomIA());
 			
+	}
+	
+	@Test
+	public void testGetRandomDeck()
+	{
+		resetBuildings();
+		// Test d'initialisation
+		assertEquals(Settings.BUILDINGS.size(), Settings.NB_BUILDING_CARD_IN_DECK * Settings.NB_BUILDING_DECK);
+		for (int i = 0; i < Settings.NB_BUILDING_DECK; i++)
+		{
+			// Test sur la taille de chaque ArrayList
+			assertEquals(Settings.getRandomDeck().size(), Settings.NB_BUILDING_CARD_IN_DECK);
+		}
+		// Test sur une liste vide
+		assertEquals(Settings.getRandomDeck().size(), 0);
+	}
+	
+	public void resetBuildings ()
+	{
+		Settings.BUILDINGS = new ArrayList<Building>(Arrays.asList(
+				new BuildingRessourceImposed(10, Ressource.WOOD, Ressource.WOOD, Ressource.CLAY),
+				new BuildingRessourceImposed(11, Ressource.WOOD, Ressource.WOOD, Ressource.STONE),
+				new BuildingRessourceImposed(12, Ressource.WOOD, Ressource.WOOD, Ressource.GOLD),
+				new BuildingRessourceImposed(11, Ressource.WOOD, Ressource.CLAY, Ressource.CLAY),
+				new BuildingRessourceImposed(12, Ressource.WOOD, Ressource.CLAY, Ressource.STONE),
+				new BuildingRessourceImposed(12, Ressource.WOOD, Ressource.CLAY, Ressource.STONE),
+				new BuildingRessourceImposed(13, Ressource.CLAY, Ressource.CLAY, Ressource.STONE),
+				new BuildingRessourceImposed(14, Ressource.WOOD, Ressource.STONE, Ressource.GOLD),
+				new BuildingRessourceImposed(14, Ressource.WOOD, Ressource.STONE, Ressource.GOLD),
+				new BuildingRessourceImposed(13, Ressource.WOOD, Ressource.STONE, Ressource.STONE),
+				new BuildingRessourceImposed(13, Ressource.WOOD, Ressource.CLAY, Ressource.GOLD),
+				new BuildingRessourceImposed(13, Ressource.WOOD, Ressource.CLAY, Ressource.GOLD),
+				new BuildingRessourceImposed(16, Ressource.STONE, Ressource.STONE, Ressource.GOLD),
+				new BuildingRessourceImposed(15, Ressource.CLAY, Ressource.STONE, Ressource.GOLD),
+				new BuildingRessourceImposed(15, Ressource.CLAY, Ressource.STONE, Ressource.GOLD),
+				new BuildingRessourceImposed(14, Ressource.CLAY, Ressource.STONE, Ressource.STONE),
+				new BuildingRessourceImposed(14, Ressource.CLAY, Ressource.CLAY, Ressource.GOLD),
+				new BuildingRessourceChoosed(),
+				new BuildingRessourceChoosed(),
+				new BuildingRessourceChoosed(),
+				new BuildingRessourceNotImposed(2, 5),
+				new BuildingRessourceNotImposed(1, 5),
+				new BuildingRessourceNotImposed(4, 4),
+				new BuildingRessourceNotImposed(3, 4),
+				new BuildingRessourceNotImposed(2, 4),
+				new BuildingRessourceNotImposed(1, 4),
+				new BuildingRessourceNotImposed(4, 5),
+				new BuildingRessourceNotImposed(3, 5)
+				));
 	}
 }
