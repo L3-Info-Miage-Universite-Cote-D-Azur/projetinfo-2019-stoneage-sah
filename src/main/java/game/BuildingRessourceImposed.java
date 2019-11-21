f916342f59e2075aa33d262c727713018adf7412
theoricien <maxime.jerome@etu.unice.fr>
2019-11-21 16:45:53 +0100

    Ajout de checkNeededRessource() #52
    #52

--- src/main/java/game/BuildingRessourceImposed.java
+++ src/main/java/game/BuildingRessourceImposed.java
@@ -16,4 +16,20 @@ public class BuildingRessourceImposed extends Building
 			super.neededRessource[i] = r[i];
 		}
 	}
+	
+	/**
+	 * Verifie si le joueur peut retirer la carte et ses points
+	 * @param ressources copie de l'inventaire du joueur
+	 * @return true si il peut, false sinon
+	 */
+	public boolean checkNeededRessource (int[] ressources)
+	{
+		for (int i = 0; i < super.neededRessource.length; i++)
+		{
+			ressources[super.neededRessource[i].getIndex()] -= 1;
+			if (ressources[super.neededRessource[i].getIndex()] < 0)
+				return false;
+		}
+		return true;
+	}
 }
