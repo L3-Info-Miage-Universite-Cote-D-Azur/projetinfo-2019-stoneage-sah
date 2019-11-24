fcaa29c725595dfef35f022794d9a2420c4e0d69
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-24 22:49:53 +0100

    Restructuration du code #77
    Fait avec yohann Tognetti

---
+++ src/main/java/game/building/Building.java
@@ -0,0 +1,35 @@
+package game.building;
+
+import game.Ressource;
+
+/**
+ * Les Building sont les batiments, ils sont caracterises par leur type:
+ * -1: abstract Building
+ * 0: BuildingRessourceImposed: batiment dont les points de victoire et ressources sont imposes
+ * 1: BuildingRessourceNotImposed: batiment dont les ressources ne sont pas imposees, mais compte un nombre de ressources differentes a poser
+ * 2: BuildingRessourceChoosed: batiment unique, il permet de deposer entre 1 et 7 ressources
+ */
+public abstract class Building 
+{	
+	/* FIELDS */
+	protected int victoryPoints;
+	// Tableau des ressources misees par le joueur
+	protected Ressource[] neededRessource;
+	protected int type;
+	
+	/* CONSTRUCTOR */
+	public Building (int type)
+	{
+		this.type = type;
+	}
+	
+	/* GETTERS */
+	public int getVP () {return this.victoryPoints;}
+	public Ressource[] getNeededRessource() {return this.neededRessource;}
+	public int getType() {return this.type;}
+	
+	/* SETTERS */
+	protected void setVP (int VP) {this.victoryPoints = VP;}
+	protected void setNeededRessource (Ressource[] r) {this.neededRessource = r;}
+	protected void setType (int t) {this.type = t;}
+}
---
+++ src/main/java/game/building/BuildingRessourceImposed.java
@@ -0,0 +1,37 @@
+package game.building;
+
+import game.Ressource;
+
+/**
+ * Batiment qui contient directement un nombre de ressources imposees et un nombre de point de victoire connu
+ */
+public class BuildingRessourceImposed extends Building 
+{
+	/* CONSTRUCTOR */
+	public BuildingRessourceImposed(int vP, Ressource... r)
+	{
+		super(0);
+		super.victoryPoints = vP;
+		super.neededRessource = new Ressource[r.length];
+		for (int i = 0; i < r.length; i++)
+		{
+			super.neededRessource[i] = r[i];
+		}
+	}
+	
+	/**
+	 * Verifie si le joueur peut retirer la carte et ses points
+	 * @param ressources copie de l'inventaire du joueur
+	 * @return true si il peut, false sinon
+	 */
+	public boolean checkNeededRessource (int[] ressources)
+	{
+		for (int i = 0; i < super.neededRessource.length; i++)
+		{
+			ressources[super.neededRessource[i].getIndex()] -= 1;
+			if (ressources[super.neededRessource[i].getIndex()] < 0)
+				return false;
+		}
+		return true;
+	}
+}
