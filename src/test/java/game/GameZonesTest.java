76ccbfe170ba5342c38ebc2943f1a20c96a75db3
iopama <56683098+iopama@users.noreply.github.com>
2019-11-28 21:59:23 +0100

    Update de GameZonesTest
    Concerned: #108

--- src/test/java/game/GameZonesTest.java
+++ src/test/java/game/GameZonesTest.java
@@ -6,23 +6,19 @@ import org.junit.jupiter.api.Test;
 
 import inventory.Inventory;
 import player.Player;
+import game.GamePlayers;
 
 class GameZonesTest{
 	Dice d;
 	GameZones g = new GameZones(2, d);
+	GamePlayers gh = new GamePlayers(4);
 	Inventory i = new Inventory();
 	Player p = new Player("sardoche", i.getInventoryIA());
 	
 	@Test
 	void testPlayerHarvest() {
-		g.playerHarvest(p, i);
+		g.playerHarvest(p, i, gh);
 		assertEquals(p.getCurrentFigurine(),p.getMaxFigurine());
-	}
-	
-	@Test
-	void testPlaceFigurine() {
-		g.playerPlaceFigurine(p);
-		assertEquals(true, p.getCurrentFigurine()>=0);
 	}
 
 }
