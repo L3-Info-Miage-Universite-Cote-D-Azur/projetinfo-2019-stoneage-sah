9801d0978c7e11aa8b34a4cc2a018984154f4882
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-28 16:16:40 +0100

    Mise a jour des Tests

--- src/test/java/client/RandomIATest.java
+++ src/test/java/client/RandomIATest.java
@@ -1,19 +1,25 @@
 package client;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
+import game.Ressource;
+import inventory.Inventory;
+import player.Player;
 
 
 public class RandomIATest {
-	public IA testIa = new RandomIA();
-	
+	public Inventory inv = new Inventory();
+	public IA testIa = new RandomIA(null,inv.getInventoryIA());
+
 	@Test
 	void testChooseZone() {
 		for(int i = 1; i <= 10; i++) {
 			//On incremente la taille du tableau a chaque iteration. 
 			int[] tab1 = new int[i];
 			String[] tab2 = new String[i];
-			
+
 			for(int j =0; j < 100; j++) {
 				//On test 100 fois que l'indice renvoye est bien compris entre 0 et tab1.length
 				int test = testIa.chooseZone(tab1, tab2);
@@ -22,7 +28,7 @@ public class RandomIATest {
 			}
 		}
 	}
-	
+
 	@Test
 	void testChooseNumber() {
 		for(int j =0; j < 100; j++) {
@@ -30,154 +36,172 @@ public class RandomIATest {
 			int test = testIa.chooseNumber(1, 7);
 			assertEquals(true,test <= 7);
 			assertEquals(true,test >= 1);
-			
+
 			int test2 = testIa.chooseNumber(1, 2);
 			assertEquals(true,test2 <= 7);
 			assertEquals(true,test2 >= 1);
-			
+
 			int test3 = testIa.chooseNumber(2, 2);
 			assertEquals(true,test3 <= 7);
 			assertEquals(true,test3 >= 1);
 		}
 	}
-	
+
 	@Test
 	void testChooseRessource() {
+		//CAS 1
+		inv.addRessource(Ressource.WOOD, 1);
+		inv.addRessource(Ressource.CLAY, 2);
+		inv.addRessource(Ressource.STONE, 3);
 		
-		//On test 50 fois que l'indice renvoye est bien compris en 0 et tab.length
-		//On test 50 fois que le nombre recupere est bien entre 1 et 3.
-		int[] tab1 = new int[] {1,2,3};
-		String[] tab2 = new String[3];
-		int i1 = 5;
-			
+		int i1 = 3;
+		
 		for(int j =0; j < 50; j++) {
-			int[] test = testIa.chooseRessource(i1, tab1, tab2);
-			assertEquals(true,test[0] < tab1.length);
+			int[] test = testIa.chooseRessource(i1);
+			assertEquals(true,test[0] < 3);
 			assertEquals(true,test[0] >= 0);
 			assertEquals(true,test[1] > 0);
 			assertEquals(true,test[1] <= 3);
-			
-			assertEquals(false,test[0] >= tab1.length);
+
+			assertEquals(false,test[0] >= 3);
 			assertEquals(false,test[0] < 0);
 			assertEquals(false,test[1] < 0);
 			assertEquals(false,test[1] > 3);
 		}
 		
-		//On test 50 fois que l'indice renvoye est bien compris en 0 et tab.length
-		//On test 50 fois que le nombre recupere est bien 1.
-		int[] tab3 = new int[] {1,1,1,1,1};
-		String[] tab4 = new String[5];
+		
+		//CAS 2
+		inv = new Inventory();
+		testIa = new RandomIA(null,inv.getInventoryIA());
+		
+		inv.addRessource(Ressource.WOOD, 1);
+		inv.addRessource(Ressource.CLAY, 1);
 		int i2 = 2;
-			
+
 		for(int j =0; j < 50; j++) {
-			int[] test = testIa.chooseRessource(i2, tab3, tab4);
-			assertEquals(true,test[0] < tab3.length);
+			int[] test = testIa.chooseRessource(i2);
+			assertEquals(true,test[0] < 2);
 			assertEquals(true,test[0] >= 0);
 			assertEquals(true,test[1] == 1);
-			
-			assertEquals(false,test[0] >= tab3.length);
+
+			assertEquals(false,test[0] >= 2);
 			assertEquals(false,test[0] < 0);
 			assertEquals(false,test[1] < 0);
 			assertEquals(false,test[1] >= 2);
 		}
+
+		//CAS 3
+		inv = new Inventory();
+		testIa = new RandomIA(null,inv.getInventoryIA());
 		
-		//On test 50 fois que l'indice renvoye est bien compris en 0 et tab.length
-		//On test 50 fois que le nombre recupere est bien entre 1 et i3.
-		int[] tab5 = new int[] {5,6,7,4};
-		String[] tab6 = new String[4];
+		inv.addRessource(Ressource.WOOD, 3);
 		int i3 = 3;
-			
+
 		for(int j =0; j < 50; j++) {
-			int[] test = testIa.chooseRessource(i3, tab5, tab6);
-			assertEquals(true,test[0] < tab5.length);
-			assertEquals(true,test[0] >= 0);
+			int[] test = testIa.chooseRessource(i3);
+			assertEquals(true,test[0] == 0);
 			assertEquals(true,test[1] > 0);
 			assertEquals(true,test[1] <= i3);
-			
-			assertEquals(false,test[0] >= tab5.length);
-			assertEquals(false,test[0] < 0);
+
+			assertEquals(false,test[0] != 0);
 			assertEquals(false,test[1] < 0);
 			assertEquals(false,test[1] > i3);
 		}
 	}
-	
+
 	@Test 
 	void testPickCard() {
-		
+
 		//Test dans un cas ou il dois depenser toutes ses ressources. 
-		int[] ressource = new int[]{0,0,0,4};
+		inv = new Inventory();
+		testIa = new RandomIA(null,inv.getInventoryIA());
+		
+		inv.addRessource(Ressource.GOLD, 4);
+		
 		int number = 4;
 		int[] res;
-		
+
 		for(int j =0; j < 50; j++) {
-			res = testIa.pickCard(ressource.clone(), number);
-			
+			res = testIa.pickCard(number);
+
 			assertEquals(true,res[0] == 0);
 			assertEquals(true,res[1] == 0);
 			assertEquals(true,res[2] == 0);
 			assertEquals(true,res[3] == 4 || res[3] == 0);
 		}
-		
+
 		//Test dans un cas general
-		ressource = new int[] {1,2,3,4};
-		number = 5;
+		inv = new Inventory();
+		testIa = new RandomIA(null,inv.getInventoryIA());
 		
+		inv.addRessource(Ressource.WOOD, 1);
+		inv.addRessource(Ressource.CLAY, 2);
+		inv.addRessource(Ressource.STONE, 3);
+		inv.addRessource(Ressource.GOLD, 4);
+		number = 5;
+
 		for(int j =0; j < 50; j++) {
-			res = testIa.pickCard(ressource.clone(), number);
-			
+			res = testIa.pickCard(number);
+
 			assertEquals(true,res[0] >= 0 && res[0] <= 1);
 			assertEquals(true,res[1] >= 0 && res[1] <= 2);
 			assertEquals(true,res[2] >= 0 && res[2] <= 3);
 			assertEquals(true,res[3] >= 0 && res[3] <= 4);
-			
+
 			int sum = 0;
 			for(int el : res) {
 				sum += el;
 			}
 			assertEquals(true, sum == number || sum == 0);
 		}
-		
+
 		//Le cas ou l'ia n'a pas assez de ressource est tester en amont et donnerais une boucle infinie ici. 
 	}
-	
+
 	@Test
 	void pickTools(){
-		
+
 		//Cas ou il n'as pas d'outils
-		int[] tools = new int[] {0,0,0};
-		boolean [] toolDispo = new boolean[] {true,true,true};
 		boolean[] res;
-		
+
 		for(int j =0; j < 50; j++) {
-			res = testIa.pickTools(tools, toolDispo);
-			
+			res = testIa.pickTools();
+
 			assertEquals(true, res[0] == false);
 			assertEquals(true, res[1] == false);
 			assertEquals(true, res[2] == false);
 		}
 		
 		//Cas general 
-		tools = new int[] {2,2,1};
-		toolDispo = new boolean[] {false, true, false};
+		inv = new Inventory();
+		testIa = new RandomIA(null,inv.getInventoryIA());
+		Player testPlayer = new Player("test",inv.getInventoryIA());
 		
-		for(int j =0; j < 50; j++) {
-			res = testIa.pickTools(tools, toolDispo);
-			
-			assertEquals(true, res[0] == false);
-			assertEquals(true, res[1] == false || res[1] == true);
-			assertEquals(true, res[2] == false);
+		for(int i =0; i<5;i++) {
+			inv.getTools().incrementTool();
 		}
-		
-		//Cas general 2
-		tools = new int[] {1,0,0};
-		toolDispo = new boolean[] {true, true, true};
-		
+		inv.getTools().useTools(testPlayer, 0, null);
+		boolean[] testBooleans = inv.getTools().getToolsUsed();
+
 		for(int j =0; j < 50; j++) {
-			res = testIa.pickTools(tools, toolDispo);
+			res = testIa.pickTools();
 			
-			assertEquals(true, res[0] == false || res[0] == true);
-			assertEquals(true, res[1] == false);
-			assertEquals(true, res[2] == false);
+			if(testBooleans[0]) {
+				assertEquals(true, res[0] == false);
+			}else {
+				assertEquals(true, res[0] == false || res[0] == true);
+			}
+			if(testBooleans[1]) {
+				assertEquals(true, res[1] == false);
+			}else {
+				assertEquals(true, res[1] == false || res[1] == true);
+			}
+			
+			if(testBooleans[2]) {
+				assertEquals(true, res[2] == false);
+			}else {
+				assertEquals(true, res[2] == false || res[2] == true);
+			}
 		}
 	}
 }
