4a8dd4437f1d7d5128e7afb09c4329d104039fef
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-11-28 12:04:15 +0100

    Restructuration #77
    fait avec Yohann Tognetti

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,33 +1,41 @@
 import game.Game;
+import printer.Printer;
 
 public class Program {
 	/**
 	 * Cette classe contient le main et lance la partie
 	 */
 
-    public static void main(String[] args) {
-    	//System.setProperty( "file.encoding", "UTF-8" );
-    	System.out.println("\n######## L'AGE DE PIERRE ########\n");
-    	int defaultNumber = 4;
-    	int numberPlayer;
-    	if(args.length==0) { numberPlayer=defaultNumber; }
-    	else{
-    		try{
-    			numberPlayer = Integer.parseInt(args[0]);
-    		}catch(Exception e) {
-    			System.out.println("Choix de joueur invalide : "+e+" il est donc automatiquement initialiser a 4");
-    			numberPlayer = defaultNumber;
-    		}
-    	}
-    	Game game;
-    	if(numberPlayer>1 && numberPlayer<=4) {
-    		game= new Game(numberPlayer);
-    	}
-    	else { 
-    		System.out.println("Choix de joueur invalide il est donc automatiquement initialiser a "+defaultNumber);
-    		numberPlayer = defaultNumber;
-    		game= new Game(numberPlayer);
-    	}
-    	game.gameLoop();
-    }
+	public static void main(String[] args) {
+		//System.setProperty( "file.encoding", "UTF-8" );
+		
+		//si on veut que l'affichage soit active ou non
+		//true : affichage / false : pas d'affichage
+		Printer.Instance(true);
+		
+		Printer.getPrinter().println("\n######## L'AGE DE PIERRE ########\n");
+		int defaultNumber = 4;//Nombre de joueurs par defaut.
+		int numberPlayer;
+		if(args.length==0) { numberPlayer=defaultNumber; }//Si le programme est appelle sans argument.
+		else{
+			try{
+				numberPlayer = Integer.parseInt(args[0]);
+				
+			}catch(Exception e) {
+				//Choix invalide lors de l'appel du programme. 
+				Printer.getPrinter().println("Choix de joueur invalide : "+e+" il est donc automatiquement initialiser a 4");
+				numberPlayer = defaultNumber;
+			}
+		}
+		Game game;
+		if(numberPlayer>1 && numberPlayer<=4) {
+			game= new Game(numberPlayer);//Initialisation du jeu.
+		}
+		else { 
+			Printer.getPrinter().println("Choix de joueur invalide il est donc automatiquement initialiser a "+defaultNumber);
+			numberPlayer = defaultNumber;
+			game= new Game(numberPlayer);//Initialisation du jeu.
+		}
+		game.gameLoop();//Lancement du jeu.
+	}
 }
