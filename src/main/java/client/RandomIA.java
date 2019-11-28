226ec47f2351a713621695913e240904c97cd64f
Idrash <56682531+Idrash@users.noreply.github.com>
2019-11-28 20:48:36 +0100

    correction use tools #79

--- src/main/java/client/RandomIA.java
+++ src/main/java/client/RandomIA.java
@@ -1,37 +1,29 @@
 package client;
 
 import java.util.Random;
-import java.util.stream.IntStream;
 
 import game.Ressource;
 import game.Settings;
-import game.building.*;
-import game.zone.*;
+import game.zone.ZoneBuilding;
+import game.zone.ZoneCarteCivilisation;
 import inventory.InventoryIA;
 import player.PlayerIA;
 
 
 /**
- * Priorite decroissante:
- * ZoneHunt:		[4]
- * ZoneField: 		[5]
- * ZoneHut: 		[6]
- * ZoneTool:		[7]
- * ZoneBuildings:	[12-15]
- * ZoneCarteCiv:	[8-11]
- * ZoneRessource:	[0-3]
+ * RandomIA correspond a l'IA comportant une strategie basee sur le hasard. 
+ * @author Mentra20
+ *
  */
 
-public class NoobIA extends IA 
-{
-    public NoobIA (PlayerIA playerIA, InventoryIA inventoryIA) 
-    {
-		super(playerIA, inventoryIA);
+public class RandomIA extends IA {
+
+	public RandomIA(PlayerIA playerIA, InventoryIA inventoryIA) {
+		super(playerIA,inventoryIA);
 	}
 
 	//Random de l'IA (elle ne peux pas acceder a Settings).
-    private Random rand = new Random();
-    private int currentZone;
+	private Random rand = new Random();
 
     /**
 	 * chooseZone retourne l'indice de la zone choisie par l'IA . 
@@ -43,273 +35,123 @@ public class NoobIA extends IA
 	 */
     public int chooseZone (int[] zoneAvailableSpace, String[] zoneName, ZoneBuilding[] buildings, ZoneCarteCivilisation[] cV)
     {
-    	// RESET
-    	this.currentZone = -1;
-    	// SI ON A PAS ASSEZ POUR NOURRIR TLM A LA FIN DU TOUR
-        if (this.inventoryIA.getRessource(Ressource.FOOD) < this.playerIA.getMaxFigurine() + this.inventoryIA.getRessource(Ressource.FIELD))
-        {
-        	// SI AVEC 1 DE FIELD ON PEUT SURVIVRE
-        	if (this.playerIA.getMaxFigurine() + this.inventoryIA.getRessource(Ressource.FIELD) + 1 == this.inventoryIA.getRessource(Ressource.FOOD))
-        	{
-        		// SI LA FERME EST DISPO
-        		if (zoneAvailableSpace[5] == 1)
-                {
-        			if (this.rand.nextInt(2) == 1) this.currentZone = 5;
-                }
-        		// SINON ON CHECK TOUTES LES CARTES CIVILISATION
-        		else
-        		{
-        			for (int i = 0; i < cV.length; i++)
-        			{
-        				// SI ELLE OFFRE 1 MARQUEUR NOURRITURE
-        				if (cV[i].getCard().getRessource() == Ressource.FIELD)
-        				{
-        					if (this.rand.nextInt(2) == 1) this.currentZone = i+8;
-        				}
-        			}
-        			// SINON ON VA DANS LA CHASSE
-        			if (this.currentZone == -1) 
-        				this.currentZone = 4;
-        		}
-        		// SINON ON VA DANS LA CHASSE
-    			if (this.currentZone == -1) 
-    				this.currentZone = 4;
-        	}
-        }
-        // SI ON A ASSEZ DE NOURRITURE, ON SE CONCENTRE D'ABORD SUR LE VILLAGE
-        else 
-       	{
-       		if (zoneAvailableSpace[5] >= 1)
-       		{
-       			if (this.rand.nextInt(3) == 1) this.currentZone = 5;
-       		}
-       		else if (zoneAvailableSpace[6] >= 1)
-       		{
-       			if (this.rand.nextInt(3) == 1) this.currentZone = 6;
-       		}
-       		else if (zoneAvailableSpace[7] >= 1)
-       		{
-       			if (this.rand.nextInt(3) == 1) this.currentZone = 7;
-       		}
-       		
-       		// SI LE VILLAGE EST PLEIN, ON SE CONCENTRE SUR LE GAIN DE POINTS, ET SUR LES RESSOURCES NECESSAIRES
-       		else
-       		{
-       			// CHECK DES CARTES BATIMENT
-       			for (int i = 0; i < buildings.length; i++)
-       			{
-       				// SI Y'A DE LA PLACE
-       				if (zoneAvailableSpace[i] == 1)
-       				{
-           				Building tmpBuilding = buildings[i].getBuilding();
-           				// RESSOURCE IMPOSED
-           				if (tmpBuilding.getType() == 0)
-               			{
-           					// SI C'EST UN BUILDING AVEC RESSOURCE IMPOSEES ET QU'ON A LES RESSOURCES
-           					if (((BuildingRessourceImposed) tmpBuilding).checkNeededRessource(this.inventoryIA.getCopyRessources()))
-           					{
-           						if (this.rand.nextInt(2) == 1) this.currentZone = i + 12;
-           					}
-               			}
-           				else if (tmpBuilding.getType() == 1)
-           				{
-           					// SI C'EST UN BUILDING AVEC RESSOURCE NON IMPOSEES ET QU'ON A LES RESSOURCES
-           					if (((BuildingRessourceNotImposed) tmpBuilding).checkNeededRessource())
-           					{
-           						if (this.rand.nextInt(2) == 1) this.currentZone = i + 12;
-           					}
-           				}
-           				else if (tmpBuilding.getType() == 2)
-           				{
-           					// SI C'EST UN BUILDING AVEC RESSOURCE A CHOIX ET QU'ON A LES RESSOURCES
-           					if (((BuildingRessourceChoosed) tmpBuilding).checkNeededRessource())
-           					{
-           						if (this.rand.nextInt(2) == 1) this.currentZone = i + 12;
-           					}
-           				}
-           				// AUCUN AUTRE BATIMENT POSSIBLE
-           				else {throw new RuntimeException("Building type not expected");}
-       				}
-       			}
-       			
-        		if (this.currentZone == -1)
-        		{
-        			// REGARDER SI ON PEUT PRENDRE UNE CARTE CIVILISATION
-        			for (int i = 0; i < cV.length; i++)
-        			{
-        				// SI ON A ASSEZ DE RESSOURCE
-        				if (IntStream.of(this.inventoryIA.getCopyRessources()).sum() >= 4 - i && zoneAvailableSpace[i] == 1)
-        				{
-        					if (this.rand.nextInt(2) == 1) this.currentZone = i + 8;
-        					break;
-        				}
-        			}
-        		}
-        			
-        		// IL NOUS RESTE LES RESSOURCES
-        		if (this.currentZone == -1)
-        		{
-        			for (int i = 0; i < 3; i++)
-        			{
-        				if (zoneAvailableSpace[i] >= 1)
-        				{
-        					if (this.rand.nextInt(2) == 1) this.currentZone = i;
-        					break;
-        				}
-        			}
-        		}
-        		
-        		// A LA FIN, IL RESTE LA CHASSE
-        		if (this.currentZone == -1)
-        			this.currentZone = 4;
-        	}
-       		// SINON ON VA DANS LA CHASSE
-			if (this.currentZone == -1) 
-				this.currentZone = 4;
-        }
-        // SINON ON VA DANS LA CHASSE
-     	if (this.currentZone == -1) 
-     		this.currentZone = 4;
-     	
-     	System.out.println(this.currentZone);
-        return this.currentZone;
-    }
+		return rand.nextInt(zoneAvailableSpace.length);
+	}
 
-    /**
-     * chooseNumber retourne le nombre de figurines choisie par l'IA (ici au hasard). 
-     * @param min et max, le nombre minimum et maximum entre lesquels l'IA doit choisir. 
-     * @return l'indice de la zone
-     */
-    public int chooseNumber (int min, int max) 
-    {
-    	// CE JOUEUR SUPPOSE QUE 2 FIGURINES = 3 DE NOURRITURE
-        if (this.currentZone == 4)
-        {
-        	int nbFoodRequired = ((this.inventoryIA.getRessource(Ressource.FIELD) + this.inventoryIA.getRessource(Ressource.FOOD)) - this.playerIA.getMaxFigurine());
-        	if (((2 * nbFoodRequired) / 3) % max < min)
-        		return min;
-        	return ((2 * nbFoodRequired) / 3) % max;
-        }
-        // SINON IL RENVOIE UN NOMBRE ALEATOIRE
-        return this.rand.nextInt(max - min + 1) + min;
-    }
-    
-    /**
-     * chooseRessource retourne le nombre de ressource choisie par l'IA au hasard. 
-     * @param Le nombre de figurines a nourrir
-     * @param Un tableau contenant le nombre de ressource que l'IA possede par index.
-     * @param Un tableau contenant le nom des ressources que l'IA possede par index.
-     * @return l'indice de la zone
-     */
-    public int[] chooseRessource(int figurinesToFeed) {
-        int ressource;
-        int index = -1;
-        // BOIS - ARGILE - PIERRE - OR
-        do {
-            index++;
-            ressource = inventoryIA.getRessource(Ressource.indexToRessource(index));
-        }while(ressource==0);
-        
-        int nb = this.rand.nextInt(Math.min(ressource, figurinesToFeed))+1;
-        
-        int[] res = new int[] {index,nb};
-        return res;
-    }
+	/**
+	 * chooseNumber retourne le nombre de figurines choisie par l'IA. 
+	 * @param min : le nombre de figurines minimum.
+	 * @param max : le nombre de figurines maximum. 
+	 * @return l'indice de la zone
+	 */
+	public int chooseNumber (int min,int max) {
+		return rand.nextInt(max-min+1)+min;
+	}
 
-    /**
-     * useRessourceToFeed renvoie true ou false selon le choix de l'IA pour nourrir 
-     * ses figurines avec des ressources. Le choix se fait au hasard.  
-     * @param ressourceNumber Le tableau du nombre de ressource possedee par le joueur. 
-     * @return Booleen : true si le joueur veut utiliser ses ressources pour nourrir, false sinon.
-     */
-    public boolean useRessourceToFeed()
-    {
-    	// LE JOUEUR PRIVILEGIE LE SCORE
-        return true;
-    }
-    
-    /**
-     * pickCard renvoie les ressources donnee pour l'achat de la carte civilisation. 
-     * Si l'IA renvoie un tableau vide, elle refuse l'achat de la carte. 
-     * @param ressourceNumber : les ressources premieres du joueur
-     * @param numberRessourceRequire : le nombre de ressource pour l'achat de la carte. 
-     * @return res : tableau des ressources donnes pour la carte. 
-     */
-    public int[] pickCard(int numberRessourceRequire) 
-    {    
-        int[] res = new int[] {0,0,0,0};
-        int[] ressourceNumber = new int[4];
-        for(int i = 0; i<4; i++) {
+	/**
+	 * chooseNumber retourne le nombre de ressource choisie par l'IA au hasard. 
+	 * @param Le nombre de figurines a nourrir
+	 * @return l'indice de la zone
+	 */
+	public int[] chooseRessource(int figurinesToFeed) {
+		int ressource;
+		int index;
+		do {
+			index = rand.nextInt(4);
+			ressource = inventoryIA.getRessource(Ressource.indexToRessource(index));
+		}while(ressource == 0);
+
+		int nb = rand.nextInt(Math.min(ressource, figurinesToFeed))+1;
+
+		int[] res = new int[] {index,nb};
+		return res;
+	}
+
+	/**
+	 * useRessourceToFeed renvoie true ou false selon le choix de l'IA pour nourrir 
+	 * ses figurines avec des ressources. Le choix se fait au hasard.  
+	 * @param ressourceNumber Le tableau du nombre de ressource possedee par le joueur. 
+	 * @return Booleen : true si le joueur veut utiliser ses ressources pour nourrir, false sinon.
+	 */
+	public boolean useRessourceToFeed() {
+		return rand.nextInt(2)==1;
+	}
+
+	/**
+	 * pickCard renvoie les ressources donnee pour l'achat de la carte civilisation. 
+	 * Si l'IA renvoie un tableau vide, elle refuse l'achat de la carte. 
+	 * @param numberRessourceRequire : le nombre de ressource pour l'achat de la carte. 
+	 * @return res : tableau des ressources donnes pour la carte. 
+	 */
+	public int[] pickCard(int numberRessourceRequire) {
+
+		int[] res = new int[] {0,0,0,0};
+		
+		int[] ressourceNumber = new int[4];
+		for(int i = 0; i<4; i++) {
 			ressourceNumber[i] = inventoryIA.getRessource(Ressource.indexToRessource(i));
 		}
-        if(this.rand.nextInt(2) == 1) {
-            while(numberRessourceRequire > 0) {
-                int index = 0;
-                // CHOISI UNE RESSOURCE: PRIORITES: bois, argile, pierre, or
-                while(index == -1 || ressourceNumber[index] == 0) {
-                    index++;
-                }
-                // CB DE RESSOURCE
-                int number = this.rand.nextInt(Math.min(numberRessourceRequire, ressourceNumber[index])) + 1;
-                ressourceNumber[index] -= number;
-                res[index] += number;
-                numberRessourceRequire -= number;
-            }
-        }
-        return res; 
-    }
-    
-    /**
-     * Renvoie un choix aleatoire sur la prise d'une carte batiment
-     * @return true ou false, aleatoirement
-     */
-    public boolean pickBuilding ()
-    {
-    	// IL JOUE LE SCORE
-        return true;
-    }
-    
-    /**
+
+		if(rand.nextInt(2) == 1) {
+			while(numberRessourceRequire > 0) {
+				int index = -1;
+				while(index == -1 || ressourceNumber[index] == 0) {
+					index = rand.nextInt(res.length);
+				}
+				int number = rand.nextInt(Math.min(numberRessourceRequire, ressourceNumber[index])) + 1;
+				ressourceNumber[index] -= number;
+				res[index] += number;
+				numberRessourceRequire -= number;
+			}
+		}
+		return res; 
+	}
+
+	/**
+	 * Renvoie un choix aleatoire sur la prise d'une carte batiment
+	 * @return true ou false, aleatoirement
+	 */
+	public boolean pickBuilding ()
+	{
+		return Settings.RAND.nextBoolean();
+	}
+
+	/**
 	 * pickTools renvoie un tableau de boolean pour dire quel outils l'IA utilise.
-	 * @param toolsToUse : le tableau des outils avec leurs niveau.
-	 * @param useTools : le tableau des outils deja utilises et non disponible.
 	 * @return
 	 */
-    public boolean[] pickTools()
-    {
-    	int[] toolsToUse = inventoryIA.getTools().getTools();
-    	boolean[] useTools = inventoryIA.getTools().getToolsUsed();
-        int usableTools=0;
-        for(int i=0;i<toolsToUse.length;i++){
-            if(useTools[i] && toolsToUse[i]>0) usableTools+=1;
-        }
-        
-        if (usableTools==0) {
-            return new boolean[]{false,false,false};
-        }
-        int numberTools = Settings.RAND.nextInt(usableTools);
-        boolean[] res = new boolean[]{false,false,false};
-        boolean hasChoose=false;
-        
-        for(int i = numberTools; 0 < i; i --){
-            while(!hasChoose){
-                int choose = this.rand.nextInt(toolsToUse.length);
-                if(useTools[choose] && toolsToUse[choose]>0 && res[choose]==false){
-                    res[choose]=true;
-                }
-            }
-        }
-        return res;
-    }
-    
-    /**
-	 * Phase de tirage
-	 * @param listeTirage des dÃ©e tirÃ©e
-	 * @param alreadyChoose si un autre joueur l'a deja choisi ou non
-	 * @return l'index de ce que veut le joueur dans le tirage
-	 */
-	public int chooseTirage(int[] listeTirage, boolean[] alreadyChoose) 
-	{
+	public boolean[] pickTools() {
+		int[] toolsToUse = inventoryIA.getTools().getTools();
+		boolean[] useTools = inventoryIA.getTools().getToolsUsed();
+		int usableTools = 0;
+		
+		for(int i = 0;i < toolsToUse.length;i++){
+			if((i>=3) || !useTools[i] && toolsToUse[i] > 0) usableTools+=1;
+		}
+
+		//cas ou il n'y a pas d'outil a utiliser
+		if (usableTools == 0) {
+			return new boolean[toolsToUse.length];
+		}
+
+		int numberTools = Settings.RAND.nextInt(usableTools+1);
+		boolean[] res = new boolean[toolsToUse.length];
+
+		for(int i = numberTools; 0 < i; i --){
+			boolean hasChoose = false;
+			while(!hasChoose){
+				int choose = Settings.RAND.nextInt(toolsToUse.length);
+				if((choose>=3 || !useTools[choose]) && toolsToUse[choose] > 0 && res[choose] == false){
+					res[choose] = true;
+					hasChoose = true;
+				}
+			}
+		}
+		return res;
+	}
+	
+	@Override
+	public int chooseTirage(int[] listeTirage, boolean[] alreadyChoose) {
 		int choose;
 		do {
 			choose = Settings.RAND.nextInt(alreadyChoose.length);
@@ -317,7 +159,11 @@ public class NoobIA extends IA
 			
 		return choose;
 	}
-    
-    public String toString () {return "Brainlet";}
+	
+
+	public String toString(){
+		return "Random";
+	}
+
 
 }
