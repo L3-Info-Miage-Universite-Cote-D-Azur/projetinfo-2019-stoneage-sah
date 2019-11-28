f948cd081c3c357496cf44b225c9f000db1d57e7
AnthonyS06 <56682383+AnthonyS06@users.noreply.github.com>
2019-11-28 18:06:26 +0100

    test de ToolStruct
    concerned : #90 , #101

---
+++ src/test/java/inventory/ToolStructTest.java
@@ -0,0 +1,56 @@
+package inventory;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ToolStructTest {
+	
+	ToolStruct ToolStructTest;
+
+	@Test
+	public void testResetToolsUsed() {
+		this.ToolStructTest = new Tools();
+		
+		//on test l'instanciation
+		assertEquals(this.ToolStructTest.getTool(),0);
+		assertEquals(this.ToolStructTest.getTools()[0], 0);
+		assertEquals(this.ToolStructTest.getTools()[1], 0);
+		assertEquals(this.ToolStructTest.getTools()[2], 0);
+		assertEquals(this.ToolStructTest.getToolsUsed()[0],false);
+		assertEquals(this.ToolStructTest.getToolsUsed()[1],false);
+		assertEquals(this.ToolStructTest.getToolsUsed()[2],false);
+		
+		this.ToolStructTest.toolsUsed[1] = true;
+		this.ToolStructTest.resetToolsUsed();
+		for (int i=0; i <this.ToolStructTest.toolsUsed.length; i++) {
+			assertEquals(this.ToolStructTest.toolsUsed[i],false);
+		}
+	}
+	
+	@Test
+	public void TestIncrementTool() {
+		this.ToolStructTest = new Tools();
+		//on test l'instanciation
+		assertEquals(this.ToolStructTest.getTool(),0);
+		assertEquals(this.ToolStructTest.getTools()[0], 0);
+		assertEquals(this.ToolStructTest.getTools()[1], 0);
+		assertEquals(this.ToolStructTest.getTools()[2], 0);
+		assertEquals(this.ToolStructTest.getToolsUsed()[0],false);
+		assertEquals(this.ToolStructTest.getToolsUsed()[1],false);
+		assertEquals(this.ToolStructTest.getToolsUsed()[2],false);
+		
+		this.ToolStructTest.incrementTool();
+		assertEquals(this.ToolStructTest.getTool(),1);
+		assertEquals(this.ToolStructTest.getTools()[0],1);
+		for (int i=1; i<3;i++) {
+			if (this.ToolStructTest.tool<12) {
+				this.ToolStructTest.incrementTool();
+				assertEquals(this.ToolStructTest.getTool(),1+i);
+				assertEquals(this.ToolStructTest.tools[(this.ToolStructTest.tool)%3]+=1,i%3);
+			}
+			
+		}
+	}
+	
+}
