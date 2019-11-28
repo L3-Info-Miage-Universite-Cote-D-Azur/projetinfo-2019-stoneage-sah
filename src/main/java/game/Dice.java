3802ab687db719714da3e71ac0f8b4f51c7722cc
theoricien <maxime.jerome@etu.unice.fr>
2019-11-28 10:26:53 +0100

    Changement des paramètres et gestion d'exceptions

--- src/main/java/game/Dice.java
+++ src/main/java/game/Dice.java
@@ -1,22 +1,30 @@
 package game;
 
+import java.util.Random;
+
 /**
  * La classe Dice represente le de non truque du plateau.  
  */
- 
-class Dice{
-    
+
+public class Dice
+{
+
     /**
      * rollDice lance number fois le de et stocke les resultats dans un tableau.
+     * @param r: instance de Random
      * @param number: le nombre de fois qu'il faut lancer le de. 
      * @return le tableau d'entier contenant les resultats du lancer de de number fois. 
      */ 
-      public static int[] rollDice(int number){
+    public static int[] rollDice(Random r, int number)
+    {
         int[] tab = new int[number];
-        
-        for(int i=0; i<number; i++) {
-        	tab[i] = Settings.RAND.nextInt(6)+1;
-        }
+
+        for(int i = 0; i < number; i++) 
+        {
+            tab[i] = r.nextInt(6)+1;
+            if (tab[i] < 1 || tab[i] > 6)
+                throw new RuntimeException("Dice returns an incompatible value");
+        }   
         return tab;
-    }
+    }   
 }
