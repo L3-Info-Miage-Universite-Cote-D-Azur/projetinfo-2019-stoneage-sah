b6d638b4ad1481bead436366699881ed1bb57272
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-12-01 18:48:13 +0100

    Changement du nombre de cartes dans le deck

--- src/test/java/game/CarteCivilisationManagerTest.java
+++ src/test/java/game/CarteCivilisationManagerTest.java
@@ -24,7 +24,7 @@ public class CarteCivilisationManagerTest {
 		
 		ccm.initDeck();
 		//Test de la longueur
-		assertEquals(true,ccm.getDeck().size() == 34);
+		assertEquals(true,ccm.getDeck().size() == 35);
 	}
 	
 	@Test
