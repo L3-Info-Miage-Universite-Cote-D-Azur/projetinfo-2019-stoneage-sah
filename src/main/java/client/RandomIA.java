1d52086dcc37b0491ec402c49bba5d3702eacc50
AnthonyS06 <56682383+AnthonyS06@users.noreply.github.com>
2019-12-05 14:39:56 +0100

    Changement mineur dans la javadoc

--- src/main/java/client/RandomIA.java
+++ src/main/java/client/RandomIA.java
@@ -49,7 +49,7 @@ public class RandomIA extends IA {
 	}
 
 	/**
-	 * chooseNumber retourne le nombre de res choisie par l'IA. 
+	 * chooseRessource retourne le nombre de ressources choisie par l'IA. 
 	 * @param Le nombre de figurines a nourrir
 	 * @return le tableau des ressources sacrifiee.
 	 */
