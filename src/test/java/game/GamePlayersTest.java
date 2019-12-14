4b834da22fda19fdbea63efee0c13275d213f482
Mentra20 <34378382+Mentra20@users.noreply.github.com>
2019-12-14 13:18:51 +0100

    Ajout de test de ressourceChoose #123

--- src/test/java/game/GamePlayersTest.java
+++ src/test/java/game/GamePlayersTest.java
@@ -1,8 +1,10 @@
 package game;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import inventory.Inventory;
+import player.Player;
 
 
 
@@ -21,5 +23,39 @@ class GamePlayersTest {
         assertEquals(true,g.getInventory(selectedPlayer).getRessource(Ressource.FOOD)==15-g.getPlayer(selectedPlayer).getCurrentFigurine());//verifie qu'il a bien enlever le nombre de nourriture correspondant aux figurines nourris
         assertEquals(true,g.getInventory(1).getRessource(Ressource.FOOD)>g.getInventory(selectedPlayer).getRessource(Ressource.FOOD));//verifie que la nourriture de joueur 1 n'a pas etait retire, montre l'independance de la fonction
     }
-
+    
+    @Test
+    void testRessourceChoose() {
+    	Inventory inventory;
+    	Player player;
+ 
+    	int nbRessourceBefore;
+    	int nbRessourceAfter;
+    	int nbRessourceGive;
+    	int nbForFeed;
+    	
+    	for(int i = 0; i < 100; i++) {
+    		inventory = new Inventory();
+    		player = new Player("test", inventory.getInventoryIA());//L'IA change a chaque nouveau test.
+    		
+    		inventory.addRessource(Ressource.STONE, 5);
+        	inventory.addRessource(Ressource.GOLD, 4);
+        	inventory.addRessource(Ressource.WOOD, 7);
+        	inventory.addRessource(Ressource.CLAY, 2);
+        	
+        	nbRessourceBefore = inventory.availableResourceToFeed();// vaut 18.
+        	nbForFeed = Settings.RAND.nextInt(18) + 1;//Le nombre de ressource que le joueur va devoir sacrifier.
+        	
+        	while(nbRessourceBefore > 0) {//Tant que le joueur n'as pas tout nourris.
+        		nbRessourceGive = g.ressourceChoose(player, inventory, nbForFeed);
+            	nbRessourceAfter = inventory.availableResourceToFeed();
+            	//Test qu'on enleve bien ce qui est renvoye.
+            	assertEquals(true, nbRessourceBefore == nbRessourceAfter + nbRessourceGive);
+            	
+            	nbRessourceBefore = nbRessourceAfter;
+        	}
+        	//Test qu'il a bien tout nourris
+        	assertEquals(0, nbRessourceBefore);
+    	}	
+    }
 }
